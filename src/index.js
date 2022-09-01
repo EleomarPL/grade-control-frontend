218554c05068151e86425a9acb18d50ed028b558
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-09-01 11:52:47 -0500

    perf: Update to react 18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
 );
 
