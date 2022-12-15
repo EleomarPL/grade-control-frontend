920769d5bc235c8bc8953261bcb2883a864cd8b3
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-12-15 12:50:05 -0600

    fix: Page login was fixed with nextui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Button, Text } from '@nextui-org/react';
 
 import useLogin from '../hooks/useLogin';
 import CustomizedInput from '../components/CustomizedInput';
 import Logo from '../components/Logo';
 import { notifyInfo } from '../consts/notify';
-import { BorderContainer, ContainerSection } from '../stylesComponents/loginStyles';
 import SpinnerLoadingButton from '../components/common/SpinnerLoadingButton';
 
 const Login = () => {
@@ -27,19 +28,21 @@ const Login = () => {
 
   return (
     <ContainerSection>
-      <BorderContainer className="d-flex flex-column" >
-        <form className="p-4 mx-4" onSubmit={ handleLogin }>
+      <BorderContainer>
+        <Form onSubmit={ handleLogin }>
           <Logo />
           <div>
-            <p>
-              <strong>Inserta tus datos, y accede para gestionar tus calificaciones</strong>
-            </p>
-            <h1
-              style={ {fontSize: '1rem', fontWeight: 'normal'} }>
+            <Text h2 size="$xl">Inserta tus datos, y accede para gestionar tus calificaciones</Text>
+            <Text h3
+              style={ {
+                fontSize: '1rem', fontWeight: 'normal',
+                padding: '5px 0'
+              } }
+            >
               Controla tus calificaciones, de una manera personalizada y en la nube
-            </h1>
+            </Text>
           </div>
-          <div className="d-flex flex-column mt-4 pt-1">
+          <div style={ { marginTop: '10px' } }>
             <CustomizedInput
               type="text" placeholder="Usuario"
               state={ userName } setState={ setUserName }
@@ -51,21 +54,50 @@ const Login = () => {
               classNameIcon="bi bi-lock-fill"
             />
           </div>
-          <div>
-            <button className="btn btn-primary mt-3 w-100" type="submit"
-              disabled={ isLoading }
-            >
-              { isLoading && <SpinnerLoadingButton /> }
-              Iniciar ahora
-            </button>
-          </div>
-          <div className="text-center pt-4" style={ {fontSize: '1rem'} }>
-            <p>Todavía no tienes cuenta? <span style={ {color: '#0004ff'} }><Link to="/register"> Registrate aquí</Link></span></p>
-          </div>
-        </form>
+          <Button shadow color="primary"
+            type="submit"
+            disabled={ isLoading }
+            css={ { px: '$13' } }
+            auto style={ { width: '100%' } }
+          >
+            { isLoading && <SpinnerLoadingButton /> }
+            Iniciar ahora
+          </Button>
+          <Text css={ { textAlign: 'center', paddingTop: '0.6rem' } }>Todavía no tienes cuenta?<Text span>
+            <Link to="/register"> Registrate aquí</Link>
+          </Text></Text>
+        </Form>
       </BorderContainer>
     </ContainerSection>
   );
 };
+
+const ContainerSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #EDEDE9;
+`;
+const BorderContainer = styled.div`
+  background: #F8F9FA;
+  width: 40%;
+
+  border: solid 1.5px #8a8a8a;
+  border-radius: 10px;
+  @media only screen and (max-width: 900px) {
+    width: 55%;
+  }
+  @media only screen and (max-width: 700px) {
+    width: 100%;
+    border: 0;
+  }
+`;
+const Form = styled.form`
+  margin: 2rem;
+  padding: 10px;
+`;
 
 export default Login;
