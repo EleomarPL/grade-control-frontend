bded838be1c68c40b5f6f0e5789fc747b36c3020
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-12-16 15:53:37 -0600

    fix: Modal to create, edit and delete score was fixed with nextui

--- src/components/ModalAddQualification.js
+++ src/components/ModalAddQualification.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { Modal, Button, Text, Input } from '@nextui-org/react';
+import { memo, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Modal } from 'bootstrap';
 
 import { validationCreateQualification } from '../services/validations/validationQualification';
 import { notifyInfo } from '../consts/notify';
@@ -8,41 +8,35 @@ import useQualification from '../hooks/useQualification';
 import SpinnerLoadingButton from './common/SpinnerLoadingButton';
 import ComponentGrouper from './common/ComponentGrouper';
 
-export const showModalStatic = () => {
-  let myModal = new Modal(
-    document.getElementById('staticBackdrop'), {
-      backdrop: 'static',
-      keyboard: true,
-      focus: true
-    }
-  );
-  myModal.show();
-};
-export const ModalAddQualification = ( { setQualifications, qualifications, dataToEdit, isCreated} ) => {
+export const ModalAddQualification = memo(function ModalAddQualification( {setQualifications, qualifications, dataToEdit, setDataToEdit, isCreated, visible, setVisible} ) {
   const [isLoading, setIsLoading] = useState(false);
+  const [course, setCourse] = useState('');
+  const [unit, setUnit] = useState('');
+  const [semester, setSemester] = useState('');
+  const [score, setScore] = useState('');
+
   const { createQualification, updateQualification } = useQualification();
 
   useEffect(() => {
-    let form = document.getElementById('form-qualification');
     if (!isCreated) {
-      
-      form[0].value = dataToEdit.course;
-      form[1].value = dataToEdit.unit;
-      form[2].value = dataToEdit.score;
-      form[3].value = dataToEdit.semester;
-    } else {
-      form[0].value = '';
-      form[1].value = '';
-      form[2].value = '';
-      form[3].value = '';
+      setCourse(dataToEdit.course);
+      setUnit(dataToEdit.unit);
+      setScore(dataToEdit.score);
+      setSemester(dataToEdit.semester);
     }
-  });
+  }, [dataToEdit]);
+  useEffect(() => {
+    if (isCreated) {
+      setCourse('');
+      setUnit('');
+      setScore('');
+      setSemester('');
+      setDataToEdit({});
+    }
+  }, [isCreated]);
 
-  const hideModalStatic = () => {
-    let myModal = Modal.getInstance(
-      document.getElementById('staticBackdrop')
-    );
-    myModal.hide();
+  const handleHideModal = () => {
+    setVisible(false);
   };
 
   const handleOperations = (evt) => {
@@ -78,14 +72,14 @@ export const ModalAddQualification = ( { setQualifications, qualifications, data
                 ...qualifications,
                 res
               ]);
-              hideModalStatic();
+              handleHideModal();
             }
           });
         } else {
           if (dataQualification.course === dataToEdit.course && Number(dataQualification.unit) === dataToEdit.unit &&
             Number(dataQualification.score) === dataToEdit.score && Number(dataQualification.semester) === dataToEdit.semester) {
             notifyInfo('Realmente no actualizo los datos, pero el trabajo esta hecho');
-            hideModalStatic();
+            handleHideModal();
           } else {
             setIsLoading(true);
             updateQualification({
@@ -103,7 +97,7 @@ export const ModalAddQualification = ( { setQualifications, qualifications, data
                   return value;
                 });
                 setQualifications(updateQualificationsLocal);
-                hideModalStatic();
+                handleHideModal();
               }
             });
           }
@@ -113,100 +107,77 @@ export const ModalAddQualification = ( { setQualifications, qualifications, data
   };
 
   return (
-    <div className="modal fade" id="staticBackdrop"
-      data-bs-backdrop="static" data-bs-keyboard="false"
-      tabIndex="-1"
-      aria-labelledby="staticBackdropLabel"
-      aria-hidden="true"
+    <Modal
+      closeButton preventClose
+      blur aria-labelledby="modal-title"
+      open={ visible } width="600px"
+      onClose={ handleHideModal }
     >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="staticBackdropLabel">
-              {
-                isCreated ? 'Crear ' : 'Editar '
-              }calificación 🌟🌟
-            </h5>
-            <button type="button" className="btn-close"
-              data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div className="modal-body">
-            <div className="pt-0 mt-0 pb-2 text-center">
-              <strong
-                style={ {fontSize: '1.5rem'} }
-              >
-                {
-                  isCreated ? 'Crear ' : 'Editar '
-                }Calificación
-              </strong>
-            </div>
-            <form onSubmit={ handleOperations } id="form-qualification">
-              <div className="d-flex flex-column pb-2">
-                <label htmlFor="course">
-                  Materia
-                </label>
-                <input type="text" className="py-3 w-100"
-                  placeholder="Ingrese el nombre de la materia"
-                  id="course"
-                />
-              </div>
-              <ComponentGrouper>
-                <div className="d-flex flex-column pb-2">
-                  <label htmlFor="unit">
-                    Unidad
-                  </label>
-                  <input type="number" className="py-3 w-100"
-                    placeholder="Ingrese la unidad"
-                    id="unit"
-                  />
-                </div>
-                <div className="d-flex flex-column pb-2">
-                  <label htmlFor="score">
-                    Calificación
-                  </label>
-                  <input type="number" className="py-3 w-100"
-                    placeholder="Ingrese la calificación - 0-100"
-                    id="score"
-                  />
-                </div>
-              </ComponentGrouper>
-              <div className="d-flex flex-column pb-2">
-                <label htmlFor="semester">
-                  Semestre
-                </label>
-                <input type="number" className="py-3 w-100"
-                  placeholder="Ingrese el semestre"
-                  id="semester"
-                />
-              </div>
-            </form>
-          </div>
-          <div className="modal-footer">
-            
-            <button
-              type="submit"
-              className="btn btn-primary"
-              form="form-qualification"
-              disabled={ isLoading }
-            >
-              { isLoading && <SpinnerLoadingButton /> }
-              Aceptar
-            </button>
-            <button
-              type="button" className="btn btn-secondary"
-              data-bs-dismiss="modal">
-              Cerrar
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Modal.Header>
+        <Text id="modal-title" size={ 18 }>
+          { `${isCreated ? 'Crear ' : 'Editar '}` }
+          <Text b size={ 18 }>
+            calificación
+          </Text>
+        </Text>
+      </Modal.Header>
+      <Modal.Body as="form" id="formqualification"
+        onSubmit={ handleOperations }
+      >
+        <Input
+          value={ course } onChange={ (e) => setCourse(e.target.value) }
+          label="Materia" placeholder="Ingresa el nombre de la materia"
+          bordered fullWidth
+        />
+        <ComponentGrouper>
+          <Input
+            value={ unit } onChange={ (e) => setUnit(e.target.value) }
+            label="Unidad" placeholder="Ingresa la unidad"
+            bordered fullWidth
+            type="number"
+          />
+          <Input
+            value={ score } onChange={ (e) => setScore(e.target.value) }
+            label="Calificación" placeholder="Calificación 0-100"
+            bordered fullWidth
+            type="number"
+          />
+        </ComponentGrouper>
+        <Input
+          value={ semester } onChange={ (e) => setSemester(e.target.value) }
+          label="Semestre" placeholder="Ingresa el semestre"
+          bordered fullWidth
+          type="number"
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button auto
+          shadow color="primary"
+          type="submit"
+          form="formqualification"
+          disabled={ isLoading }
+        >
+          { isLoading && <SpinnerLoadingButton /> }
+          Aceptar
+        </Button>
+        <Button auto flat
+          type="button"
+          shadow color="error"
+          onPress={ handleHideModal }
+        >
+          Cerrar
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
-};
+});
 
 ModalAddQualification.propTypes = {
   setQualifications: PropTypes.func.isRequired,
   qualifications: PropTypes.array.isRequired,
   isCreated: PropTypes.bool,
-  dataToEdit: PropTypes.object
+  dataToEdit: PropTypes.object,
+  visible: PropTypes.bool,
+  setVisible: PropTypes.func,
+  setDataToEdit: PropTypes.func
 };
--- src/components/ModalConfirmDelete.js
+++ src/components/ModalConfirmDelete.js
@@ -1,32 +1,19 @@
 import { useState } from 'react';
+import { Button, Modal, Text } from '@nextui-org/react';
 import PropTypes from 'prop-types';
-import { Modal } from 'bootstrap';
 
 import { notifySuccess } from '../consts/notify';
 import useQualification from '../hooks/useQualification';
 import useHistory from '../hooks/useHistory';
 import SpinnerLoadingButton from './common/SpinnerLoadingButton';
 
-export const showModalStaticDelete = () => {
-  let myModal = new Modal(
-    document.getElementById('staticBackdrop2'), {
-      backdrop: 'static',
-      keyboard: true,
-      focus: true
-    }
-  );
-  myModal.show();
-};
-export const ModalConfirmDelete = ({ idQualificationDelete, setQualifications, qualifications }) => {
+export const ModalConfirmDelete = ({ idQualificationDelete, setQualifications, qualifications, visible, setVisible }) => {
   const [isLoading, setIsLoading] = useState(false);
   const { deleteQualification } = useQualification();
   const { createHistoryDelete } = useHistory();
 
-  const hideModalStatic = () => {
-    let myModal = Modal.getInstance(
-      document.getElementById('staticBackdrop2')
-    );
-    myModal.hide();
+  const handleHideModal = () => {
+    setVisible(false);
   };
 
   const executeDelete = () => {
@@ -49,54 +36,48 @@ export const ModalConfirmDelete = ({ idQualificationDelete, setQualifications, q
           unit: temporallyObjectToDelete.unit,
           semester: temporallyObjectToDelete.semester
         });
-        hideModalStatic();
+        handleHideModal();
       }
     });
   };
   return (
-    <div className="modal fade" id="staticBackdrop2"
-      data-bs-backdrop="static" data-bs-keyboard="false"
-      tabIndex="-1"
-      aria-labelledby="staticBackdropLabel"
-      aria-hidden="true"
+    <Modal
+      closeButton preventClose
+      blur aria-labelledby="modal-delete"
+      open={ visible } width="600px"
+      onClose={ handleHideModal }
     >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="staticBackdropLabel">
-              Eliminar calificación
-            </h5>
-            <button type="button" className="btn-close"
-              data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div className="modal-body">
-            <div className="pt-0 mt-0 pb-2 text-center">
-              <strong
-                style={ {fontSize: '1.5rem'} }
-              >
-                ¿Está seguro que lo desea eliminar?
-              </strong>
-            </div>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-danger"
-              onClick={ executeDelete }
-              disabled={ isLoading }
-            >
-              { isLoading && <SpinnerLoadingButton /> }
-              Eliminar
-            </button>
-            <button
-              type="button" className="btn btn-secondary"
-              data-bs-dismiss="modal">
-              Cerrar
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Modal.Header>
+        <Text b id="modal-delete"
+          size={ 18 }
+        >
+          Eliminar calificación
+        </Text>
+      </Modal.Header>
+      <Modal.Body>
+        <Text css={ {textAlign: 'center'} } size="$2xl">
+          ¿Está seguro que lo desea eliminar?
+        </Text>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button auto
+          shadow color="error"
+          type="button"
+          disabled={ isLoading }
+          onPress={ executeDelete }
+        >
+          { isLoading && <SpinnerLoadingButton /> }
+          Eliminar
+        </Button>
+        <Button auto flat
+          type="button"
+          shadow color="primary"
+          onPress={ handleHideModal }
+        >
+          Cerrar
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
@@ -109,6 +90,8 @@ ModalConfirmDelete.propTypes = {
     PropTypes.oneOf([null]),
     PropTypes.array
   ]),
-  setQualifications: PropTypes.func.isRequired
+  setQualifications: PropTypes.func.isRequired,
+  visible: PropTypes.bool,
+  setVisible: PropTypes.func
 };
 
