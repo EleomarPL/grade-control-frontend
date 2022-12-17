4ccb5d29d5ff9aafb6abfe5fdd1ca096605e5861
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-12-16 20:04:51 -0600

    fix: Page ordenated qualification was fixed with nextui

--- src/components/OrdenatedQualification.js
+++ src/components/OrdenatedQualification.js
@@ -1,11 +1,8 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Card, Collapse, Grid, Spacer, Table, Text } from '@nextui-org/react';
 
 import { sortQualification } from '../utils/sortQualification';
-
-import {
-  DivUnit, StatusQualification
-} from '../stylesComponents/ordenatedQualificationStyles';
 
 const OrdenatedQualification = ({qualifications}) => {
   const [ordenatedQualification, setOrdenatedQualification] = useState([]);
@@ -17,63 +14,74 @@ const OrdenatedQualification = ({qualifications}) => {
   
   return (
     <>
-      {
-        ordenatedQualification.map((array, generalIndex) => {
-          if (array !== null) {
-            return <div
-              className="accordion"
-              id="accordionExample"
-              key={ generalIndex }
-            >
-              <div className="accordion-item">
-                <h2 className="accordion-header" id={ `heading${generalIndex}` }>
-                  <button className="accordion-button collapsed" type="button"
-                    data-bs-toggle="collapse" data-bs-target={ `#collapse${generalIndex}` }
-                    aria-expanded="false" aria-controls={ `collapse${generalIndex}` }>
-                    { `Semestre ${generalIndex + 1}` }
-                  </button>
-                </h2>
-                <div id={ `collapse${generalIndex}` } className="accordion-collapse collapse"
-                  aria-labelledby={ `heading${generalIndex}` } data-bs-parent="#accordionExample">
-                  <div className="accordion-body">
-                    <div className="table-responsive">
-                      <table className="table text-center">
-                        <thead className="table-dark">
-                          <tr>
-                            <th scope="col">Materia</th>
-                            <th scope="col" colSpan="17">Unidades maximas : 15</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {
-                            Object.keys(array).map((key, indexArray) => {
-                              return <tr key={ indexArray }>
-                                <td>{ key }</td>
-                                {
-                                  array[key].sort((a, b) => (a['unit'] - b['unit']) * 1).map((object, indexObject) =>
-                                    <td key={ indexObject }>
-                                      <div className="d-flex flex-column">
-                                        <DivUnit>Unidad { object.unit }</DivUnit>
-                                        <StatusQualification isApproved={ object.score > 69 }>
-                                          { object.score }
-                                        </StatusQualification>
-                                      </div>
-                                    </td>
-                                  )
-                                }
-                              </tr>;
-                            })
-                          }
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>;
-          }
-        })
-      }
+      <Collapse.Group splitted>
+        {
+          ordenatedQualification.map((array, generalIndex) => {
+            if (array !== null) {
+              return (
+                <Collapse title={ `Semestre ${generalIndex + 1}` } expanded={ generalIndex === 0 }
+                  subtitle="Has clic para ver todas las calificaciones de este semestre"
+                  key={ generalIndex }
+                >
+                  <Table
+                    aria-label="Lista de calificaciones"
+                    css={ {
+                      height: 'auto',
+                      minWidth: '100%'
+                    } }
+                  >
+                    <Table.Header>
+                      <Table.Column>Materias</Table.Column>
+                      <Table.Column>Unidades maxima: 15</Table.Column>
+                    </Table.Header>
+                    <Table.Body>
+                      {
+                        Object.keys(array).map((key, indexArray) => {
+                          return <Table.Row key={ `${indexArray}` }>
+                            <Table.Cell>{ key }</Table.Cell>
+                            <Table.Cell>
+                              <Grid.Container gap={ 2 }
+                                css={ {width: '100%'} }
+                              >
+                                <Grid sm={ 12 }>
+                                  {
+                                    array[key].sort((a, b) => (a['unit'] - b['unit']) * 1).map((object, indexObject) =>
+                                      <Card key={ indexObject }
+                                        css={ {margin: '0 10px'} }
+                                      >
+                                        <Card.Header>
+                                          <Text b>Unidad { object.unit }</Text>
+                                        </Card.Header>
+                                        <Card.Divider />
+                                        <Card.Body css={ { py: '$10' } }>
+                                          <Text
+                                            color={
+                                              `${object.score >= 70 ? 'success' : 'error'}`
+                                            }
+                                            b
+                                          >
+                                            { object.score }
+                                          </Text>
+                                        </Card.Body>
+                                      </Card>
+                                     
+                                    )
+                                  }
+                                </Grid>
+                              </Grid.Container>
+                            </Table.Cell>
+                          </Table.Row>;
+                        })
+                      }
+                    </Table.Body>
+                  </Table>
+                </Collapse>
+              );
+            }
+          })
+        }
+      </Collapse.Group>
+      <Spacer y={ 3 } />
     </>
   );
 };
